dfababdaf4462b7b14b1b1e84c243f0732ad7c51
Sumit Guha <sumit03guha@gmail.com>
2024-04-18 12:00:00 +0530

    shoes in size

---
+++ play/src/main.rs
@@ -0,0 +1,75 @@
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+fn main() {
+    let s = Some(String::from("hello"));
+
+    if let Some(_s) = &s {
+        println!("hello")
+    }
+
+    // println!("{:?}", s);
+
+    match s {
+        Some(s) if s == String::from("hejllo") => println!("asd"),
+        _ => println!("aasd")
+    }
+
+    let v1: Vec<i32> = vec![1, 2, 3];
+
+    let v2: Vec<i32> = v1.iter().map(|x| x + 1).collect();
+    println!("{:?}", v2);
+
+    let amount_to_add = 2;
+
+    let add_to_it = |x: i32| -> i32 {
+        x + amount_to_add
+    };
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
+}
