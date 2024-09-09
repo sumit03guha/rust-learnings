04b1d763db45c25a6b2efa0078702133a93de71a
Sumit Guha <sumit03guha@gmail.com>
2024-09-09 18:48:45 +0530

    multiple thread producers in a channel

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -22,6 +22,7 @@ fn main() {
     assert_eq!("Duck", message_received);
 
     multi_threads();
+    multi_prods();
 }
 
 fn multi_threads() {
@@ -53,5 +54,46 @@ fn multi_threads() {
             "a", "b", "c", "d", "e", "a", "b", "c", "d", "e", "a", "b", "c", "d", "e", "a", "b",
             "c", "d", "e", "a", "b", "c", "d", "e", "a", "b", "c", "d", "e"
         ]
+    );
+}
+
+fn multi_prods() {
+    let (tx, rx) = mpsc::channel();
+    let tx_clone = tx.clone();
+
+    let messages_to_send = vec![
+        "a", "b", "c", "d", "e", "a", "b", "c", "d", "e", "a", "b", "c", "d", "e", "a", "b", "c",
+        "d", "e", "a", "b", "c", "d", "e", "a", "b", "c", "d", "e",
+    ];
+
+    let messsages_to_send_clone = messages_to_send.clone();
+
+    thread::spawn(move || {
+        for msg in messages_to_send {
+            tx.send(msg).expect("failed to send");
+        }
+    });
+
+    thread::spawn(move || {
+        for msg in messsages_to_send_clone {
+            tx_clone.send(msg).expect("failed to send");
+        }
+    });
+
+    let mut received_messages: Vec<&str> = vec![];
+
+    for received_msgs in rx {
+        received_messages.push(&received_msgs);
+    }
+
+    assert_eq!(
+        vec![
+            "a", "b", "c", "d", "e", "a", "b", "c", "d", "e", "a", "b", "c", "d", "e", "a", "b",
+            "c", "d", "e", "a", "b", "c", "d", "e", "a", "b", "c", "d", "e", "a", "b", "c", "d",
+            "e", "a", "b", "c", "d", "e", "a", "b", "c", "d", "e", "a", "b", "c", "d", "e", "a",
+            "b", "c", "d", "e", "a", "b", "c", "d", "e",
+        ]
+        .len(),
+        received_messages.len()
     );
 }
