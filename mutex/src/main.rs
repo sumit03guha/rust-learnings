797f4e7770390d4b43935e7efaec2a0f4a87445f
Sumit Guha <sumit03guha@gmail.com>
2024-09-11 21:35:29 +0530

    atomic rc

--- mutex/src/main.rs
+++ mutex/src/main.rs
@@ -1,12 +1,44 @@
-use std::sync::Mutex;
-
-
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
 
 fn main() {
-    let m = Mutex::new(123);
+    let counter = Mutex::new(0);
+    println!("Counter init: {:?}", counter);
+
+    // thread::spawn(move || {
+    //     let mut num = value.expect("failed to take lock");
+    //     *num += 1;
+    // });
 
     {
-        let mut num = m.lock().unwrap();
-        *num = 11;
+        let mut value = counter.lock().expect("failed to take lock");
+        *value += 10;
     }
+
+    println!("Counter : {:?}", counter);
+
+    sharing_mutex_between_multiple_threads();
+}
+
+fn sharing_mutex_between_multiple_threads() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..20 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().expect("failed to take lock");
+            *num += 1;
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().expect("failed to join handles");
+    }
+
+    println!("Counter : {:?}", counter);
 }
