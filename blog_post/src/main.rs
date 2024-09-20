eebf4b5cac5009d900d770e0e48eec705b1f8d8f
Sumit Guha <sumit03guha@gmail.com>
2024-09-20 16:19:06 +0530

    oop design pattern

---
+++ blog_post/src/main.rs
@@ -0,0 +1,96 @@
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
+}
+
+struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    fn new() -> Self {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+
+    fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+    fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(self)
+    }
+    fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+    fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+}
+
+struct Draft {}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(ReviewRequested {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct ReviewRequested {}
+
+impl State for ReviewRequested {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+}
+
+fn main() {
+    let mut post = Post::new();
+
+    let text_to_add = "This is a post";
+
+    post.add_text(text_to_add);
+
+    assert_eq!(post.content(), "");
+
+    post.request_review();
+
+    assert_eq!(post.content(), "");
+
+    post.approve();
+
+    assert_eq!(post.content(), text_to_add);
+}
